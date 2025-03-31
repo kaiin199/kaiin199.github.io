4eb6f3b2c2ea18c086e2c1d55ba479ac79f2d4ba
kaiin199 <mandarine.kai@gmail.com>
2025-03-31 18:33:11 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
-const navLinks = document.querySelectorAll('.nav-link');
+//Получаем все ссылки меню 
+const navLinks = document.querySelectorAll('.nav-link')
+// Получаем все разделы страницы
 const sections = document.querySelectorAll('.section');
-
+// Обработчик клика по меню
 navLinks.forEach(link => {
-    link.addEventListener('click', (event) => {
-        event.preventDefault();
+    link.addEventListener('click', () => {
         const targetId = link.getAttribute('href').substring(1);
-         sections.forEach(section => {
-            if (section.id === targetId) {
-                section.classList.add('active');
-            } else {
-                section.classList.remove('active');
+ // Показываем только выбранный раздел
+    if (section.id === targetId) {
+        section.classList.add('active');
+    } else {
+        section.classList.remove('active');
             }
         });
     });
 });
-const animatedText = document.querySelector('.animate');
-if (animatedText) {
-    setInterval(() => {
-        animatedText.classList.toggle('fade');
-    }, 2000);
-}
